d4ec01e379d143ff7e6cd2d15864e76d133f20d9
Goran Lukic <goran.lukic@fuel.me>
2025-09-14 18:39:10 +0200

    Fix  tsconfig, gitignore and fetchData

--- src/utils/fetch-data.ts
+++ src/utils/fetch-data.ts
@@ -1,9 +1,9 @@
-export async function fetchData<T>(url:string): Promise<T | undefined> {
+export async function fetchData<T>(url:string): Promise<T> {
   try {
     const res = await fetch(url);
     const resData = (await res.json()) as T;
     return resData;
   } catch (err) {   
-    console.error(err);
+    throw err;
   }
 }
